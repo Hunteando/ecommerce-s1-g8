f1894c79433dd37bc66884312bd19bc564677b26
matias <mmaattuumza@gmail.com>
2022-11-04 16:51:04 -0300

    loggin

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,48 +1,5 @@
 import Title from './components/Title/Title';
-<<<<<<< HEAD
-import Input from './components/Input/Input';
-import Label from './components/Label/Label';
-import ButtonLogin from './components/Button/ButtonLogin';
-import Style from './Login.module.css';
-import { Footer } from '../../components/Footer/Footer';
-import { Header } from '../../components/Header/Header';
 
-const Login = () => {
-	return (
-		<>
-			<Header />
-			<section className={Style.container}>
-				<Title />
-				<div>
-					<Label texto='Usuario' />
-					<Input
-						attribute={{
-							id: 'usuario',
-							name: 'usuario',
-							type: 'texpat',
-							placeholder: 'Ingrese su usuario',
-						}}
-					/>
-				</div>
-				<div>
-					<Label texto='Contraseña' />
-					<Input
-						attribute={{
-							id: 'contraseña',
-							name: 'contraseña',
-							type: 'password',
-							placeholder: 'Ingrese su contraseña',
-						}}
-					/>
-				</div>
-				<div>
-					<ButtonLogin />
-				</div>
-				<div>
-					<Label texto='Recuperar contraseña' />
-				</div>
-			</section>
-=======
 import Style from './Login.module.css';
 import { Footer } from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
@@ -119,10 +76,10 @@ const Login = () => {
 				<input className={Style.submit} type='submit' value='Enviar' />
 
 				<div>
-					<label htmlFor="">¿Olvido su contraseña?</label>
+					<label htmlFor=''>¿Olvido su contraseña?</label>
 				</div>
 			</form>
->>>>>>> d9bb0913e227ca2ca62f18a6595a20a542ec933c
+
 			<Footer />
 		</>
 	);
